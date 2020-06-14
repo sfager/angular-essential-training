41832c2af46b32b3c087ca348874ce54d1b437cd
Stephen Fager <sfagerca82@gmail.com>
2020-06-14 12:02:09 -0400

    Completed 04_02b

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent {}
+export class MediaItemFormComponent {
+
+  onSubmit(mediaItemForm) {
+    console.log(mediaItemForm);
+  }
+}
